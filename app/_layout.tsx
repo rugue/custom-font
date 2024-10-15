2657bf09d9a7f3d78778b10cbb46d2ccea54ac91
Osarugue Enehizena <nehirugue@gmail.com>
2024-10-15 21:37:29 +0100

    Update .gitignore and package.json
    - Update .gitignore to ignore expo-generated files
    - Update package.json to use the latest versions of expo-font and expo-splash-screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,52 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [loaded, error] = useFonts({
+    "Inter-Black": require("../assets/fonts/Inter_18pt-Black.ttf"),
+    "SulphurPoint-Light": require("../assets/fonts/SulphurPoint-Light.ttf"),
+    "SulphurPoint-Bold": require("../assets/fonts/SulphurPoint-Bold.ttf"),
   });
 
   useEffect(() => {
-    if (loaded) {
+    if (loaded || error) {
       SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [loaded, error]);
 
-  if (!loaded) {
+  if (!loaded && !error) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <View style={styles.container}>
+      <Text style={styles.inter}>Inter Black</Text>
+      <Text style={styles.sulphurLight}>SulphurPoint-Light</Text>
+      <Text style={styles.sulphurBold}>SulphurPoint-Bold</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inter: {
+    fontSize: 24,
+    fontFamily: "Inter-Black",
+  },
+  sulphurLight: {
+    fontSize: 24,
+    fontFamily: "SulphurPoint-Light",
+  },
+  sulphurBold: {
+    fontSize: 24,
+    fontFamily: "SulphurPoint-Bold",
+  },
+});
